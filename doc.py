cf3f6550b480cb0021edb220d3dbfe3e14fab3ec
kaigikinyua <kaigikinyua@gmail.com>
2020-04-16 22:59:46 +0300

    Creating word documents

--- doc.py
+++ doc.py
@@ -1,16 +1,30 @@
 import os
-import docx
+from docx import Document
+from messages import Messages
+from files import Files
+
 class Documentation:
     new_documentation=True
     doc_type=""
     code_root_dir=""
     target_root_dir=""
-
+    filename=""
+    document=""
     def __init__(self,*args, **kwargs):
         doc_type=kwargs['doc_type']
         code_root_dir=kwargs['code_root_dir']
         target_root_dir=kwargs['target_root_dir']
-    
+        filename=kwargs['filename']
+        if(Files.dir_exists(target_root_dir) and Files.file_exists(str(target_root_dir)+"/"+str(filename),False)==False):
+            Messages.success("Starting documentation")
+            document=Document()
+            document.add_heading("Heading one")
+            document.add_paragraph("Paragraph one")
+            document.save(target_root_dir+"/"+str(filename))
+        else:
+            Messages.error("Check whether directory "+str(target_root_dir)+" exists")
+
+
     def check_existing_doc(self):
         pass 
 
@@ -49,4 +63,4 @@ class Comments:
     def group_paragraph(self):
         pass 
 
-D=Documentation(doc_type="Full",code_root_dir="/home/antony",target_root_dir="./home")
+D=Documentation(doc_type="Full",code_root_dir="/home/antony/Pit/Projects/desktop/autoDoc",target_root_dir="/home/antony/Desktop",filename="docone.docx")
--- files.py
+++ files.py
@@ -1,16 +1,9 @@
 import os
 
 from messages import Messages
+#CRUD file operations
 class Files:
-
-    @staticmethod
-    def file_exists(file_path):
-        if(os.path.isfile(file_path)):
-            return True
-        else:
-            Messages.error("File "+str(file_path)+" does not exist")
-            return False
-
+    #sub: Directory methods
     @staticmethod
     def dir_exists(dir_path):
         if(os.path.isdir(dir_path)):
@@ -20,8 +13,27 @@ class Files:
             return False
 
     @staticmethod
+    def create_dir(dir_path):
+        if(Files.dir_exists(dir_path)):
+            Messages.error("Directory already exists")
+        else:
+            Messages.error("Created dir method in Files has to be updated")
+
+    #endsub
+    #sub: File methods
+    #func:file_path-> Path to file, error->{Boolean Values: if true display error if any else hide error}
+    @staticmethod
+    def file_exists(file_path,error):
+        if(os.path.isfile(file_path)):
+            return True
+        else:
+            if(error==True):
+                Messages.error("File "+str(file_path)+" does not exist")
+            return False
+
+    @staticmethod
     def read_file(file_path):
-        if(Files.file_exists(file_path)):
+        if(Files.file_exists(file_path,True)):
             f=open(file_path,'r')
             data=f.readlines()
             f.close()
@@ -53,3 +65,17 @@ class Files:
         else:
             Messages.error("Could not append to file "+str(file_path))
             return False
+
+    @staticmethod
+    def delete_file(file_path):
+        if(Files.file_exists(file_path,True)):
+            try:
+                os.remove(file_path)
+                return True
+            except:
+                Messages.error("Could not delete file "+str(file_path))
+                return False
+        else:
+            Messages.error("Could not delete file "+str(file_path))
+            return False
+    #endsub
