4ff6f09cef853b7f5d3f0cf0ed398f7fbc96d933
kaigikinyua <kaigikinyua@gmail.com>
2020-05-08 23:40:11 +0300

    unit test skeleton

---
+++ src/test.py
@@ -0,0 +1,28 @@
+import test
+
+class UtilityTests:
+    def testReadFile():
+        pass
+    def testWriteFile():
+        pass
+    def testMessage():
+        pass
+    def errorMessages():
+        pass
+
+class ParserTest:
+    def testparseDir():
+        pass
+    def testparseComments():
+        pass
+
+class DocumentTest:
+    def testInput():
+        pass
+    def testHtml():
+        pass
+    def testMarkdown():
+        pass
+    def testWord():
+        pass
+    
